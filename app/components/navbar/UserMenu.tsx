57ceaa9cb72a3439856bdb4c4a79131d875418bf
AndyAND-Y <andreislatinaru@gmail.com>
2024-01-10 22:58:03 +0000

    Register UI

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,8 +4,11 @@ import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../Avatar";
 import { useCallback, useState } from "react";
 import MenuItem from "./MenuItem";
+import useRegisterModal from "@/app/hooks/useRegisterModal";
 
 const UserMenu = () => {
+
+    const registerModal = useRegisterModal();
 
     const [isOpen, setIsOpen] = useState(false);
 
@@ -90,8 +93,8 @@ const UserMenu = () => {
                             label="Login"
                         />
                         <MenuItem
-                            onClick={() => { }}
-                            label="Sing Up"
+                            onClick={registerModal.onOpen}
+                            label="Sign Up"
                         />
                     </div>
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/navbar/Navbar'
+import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './Providers/ToasterProvider'
 
 const nunito = Nunito({
     subsets: ["latin"]
@@ -21,6 +24,8 @@ export default function RootLayout({
         <html lang="en">
 
             <body className={nunito.className}>
+                <ToasterProvider />
+                <RegisterModal />
                 <Navbar />
                 {children}
             </body>
